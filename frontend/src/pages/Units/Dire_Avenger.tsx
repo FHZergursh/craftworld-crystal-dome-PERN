f16fa4fc526f53c30025f8844455939b26ad6fb2
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-05-14 20:55:16 +0100

    implement react router and tailwind, lay basic foundation for frontend
    title, pretty self explanatory

---
+++ frontend/src/pages/Units/Dire_Avenger.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dire_Avenger = () => {
+  return (
+    <div>Dire_Avenger</div>
+  )
+}
+
+export default Dire_Avenger
---
+++ frontend/src/pages/Units/Ranger.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ranger = () => {
+  return (
+    <div>Ranger</div>
+  )
+}
+
+export default Ranger
